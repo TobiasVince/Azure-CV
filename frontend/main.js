e075eb1809e614c6cd4683b1188fc628f03dc8c3
TobiasVince <126116205+TobiasVince@users.noreply.github.com>
2023-03-02 16:16:13 +0000

    changes to js

--- frontend/main.js
+++ frontend/main.js
@@ -8,12 +8,12 @@ const getVisitCount = () => {
     let count = 30;
     fetch(functionAPI).then(Response =>{
         return Response.json()
-    }).then(response => {
-        console.log("Website called function API");
+    }).then(response =>{
+        console.log("Website called function API.");
         count = response.count;
         document.getElementById("counter").innerText = count;
-    }).catch(function(error)){
+    }).catch(function(error){
         console.log(error);
-    }
+    });
     return count;
 }
